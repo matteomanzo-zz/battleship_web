fe6d53fbe483296988b42e73ed29c7cf98fdb21c
Hannah Carney <hannahcarneyart@gmail.com>
2015-01-06 13:11:42 +0000

    Working on a tab for new game

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -3,7 +3,13 @@ require 'sinatra/base'
 class BattleShips < Sinatra::Base
   get '/' do
     'Hello BattleShips!'
+     "New Game"
+
   end
+
+  get '/NewGame' do
+        
+   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
