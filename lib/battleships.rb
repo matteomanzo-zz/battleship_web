e6524abec8e14eddda46d5a1e7b1038655ecc514
Hannah Carney <hannahcarneyart@gmail.com>
2015-01-07 09:35:47 +0000

    registered two players

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -1,38 +1,19 @@
 require 'sinatra/base'
-require './lib/game.rb'
-# require './game_setup'
 
 class BattleShips < Sinatra::Base
-  set :views, Proc.new { File.join(root, "../views") }
 
-  GAME = Game.new
+  set :views, Proc.new { File.join(root, "../views") }
 
   get '/' do
     "Hello BattleShips!"
     erb :index
   end
 
-  get '/NewGame' do
-    # @game = game
-
-    @player.name = params[:name]
-    
+  get '/NewGame' do  
+    @player1 = params[:name1]
+    @player2 = params[:name2]
     erb :newgame
   end
-
-  post '/NewGame' do
-    player = Player.new
-    GAME.add_player(@player)
-    # create a player and name it 
-    redirect '/RegisterPlayerTwo'
-  end
-
-  get '/RegisterPlayerTwo' do
-    erb :register_player_two
-  end
-
-
-
   
   # start the server if ruby file executed directly
   run! if app_file == $0
