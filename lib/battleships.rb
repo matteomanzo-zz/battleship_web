edc95656cd16249727db3c0a6cd3b5fe3a265195
Hannah Carney <hannahcarneyart@gmail.com>
2015-01-07 18:22:07 +0000

    Creating the constant GAME

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -5,6 +5,9 @@ require_relative '../game_setup'
 
 class BattleShips < Sinatra::Base
 
+  GAME = Game.new
+  enable :sessions
+
   set :views, Proc.new { File.join(root, "../views") }
 
   include Helper_methods
@@ -15,17 +18,27 @@ class BattleShips < Sinatra::Base
   end
 
   get '/NewGame' do
-    @game = Game.new  
+    GAME 
     @player1 = Player.new
     @player2 = Player.new
     @player1.name = params[:name1]
     @player2.name = params[:name2]
+
     erb :newgame
   end
 
   get '/PlayGame' do
+    GAME.add_player(@player1)
+    GAME.add_player(@player2)
     @ship1 = params[:ship1]
     erb :playgame
+  end
+
+
+  get "/player" do
+    session[:player] ||= 0
+    session[:player] += 1
+    session[:player].to_s
   end
 
   
